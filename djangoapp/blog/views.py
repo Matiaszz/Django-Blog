3eb707b49984816eeb7645d10addb25a6c6851be
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-10-09 13:34:15 -0300

    Initializing the page of post

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -19,7 +19,39 @@ def index(request):
     )
 
 
-def page(request):
+def created_by(request, author_id):
+    posts = Post.objects.get_published().filter(  # type:ignore
+        created_by__id=author_id)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(  # type:ignore
+        category__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request, slug):
     return render(
         request,
         'blog/pages/page.html',
@@ -27,7 +59,13 @@ def page(request):
 
 
 def post(request, slug):
+    post = (Post.objects.get_published().filter(  # type: ignore
+        slug=slug).first())
+    context = {
+        'post': post
+    }
+
     return render(
         request,
-        'blog/pages/post.html',
+        'blog/pages/post.html', context
     )
